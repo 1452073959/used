5cb274311ef9a034d83aa968c96d0ddd03474a99
cc <1452073959@qq.com>
2020-06-20 15:22:05 +0800

    线上配置

--- config/admin.php
+++ config/admin.php
@@ -75,7 +75,7 @@ return [
     | Html title for all pages.
     |
     */
-    'title' => 'usedupload',
+    'title' => 'used',
 
     /*
     |--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return [
     | If your page is going to be accessed via https, set it to `true`.
     |
     */
-    'https' => env('ADMIN_HTTPS', false),
+    'https' => env('ADMIN_HTTPS', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -155,7 +155,7 @@ return [
     |--------------------------------------------------------------------------
     */
     'helpers' => [
-        'enable' => true,
+        'enable' => false,
     ],
 
     /*
