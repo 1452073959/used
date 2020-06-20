99737b14c57b21a83014d357e2e911a2e89ba14a
cc <1452073959@qq.com>
2020-06-20 17:32:06 +0800

    1

--- resources/lang/zh-CN/comment.php
+++ resources/lang/zh-CN/comment.php
@@ -7,8 +7,8 @@ return [
         'user_id' => '用户id',
         'pid' => '商品id',
         'parent_id' => '父级评论id',
-        'replytime' => '//评论时间',
-        'content' => '//评论内容',
+        'replytime' => '评论时间',
+        'content' => '评论内容',
     ],
     'options' => [
     ],
