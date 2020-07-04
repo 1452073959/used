3886809c7101b03cac65b2206c449ea696daca9e
1452073959@qq.com <1452073959@qq.com>
2020-07-04 17:06:14 +0800

    1

--- app/Http/Controllers/Api/QrcodeController.php
+++ app/Http/Controllers/Api/QrcodeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\Controller;
 use Illuminate\Http\Request;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use SimpleSoftwareIO\QrCode\BaconQrCodeGenerator;
@@ -11,22 +11,23 @@ class QrcodeController extends Controller
 {
     //
 
+//    public function qrcode()
+//    {
+//        $data=\App\Models\Qrcode::find(1);
+//        $url= url("/qr");
+//        //随机二维码图片名
+//        $fileName = 'my' . '.png';
+//        //二维码图片路径
+//        $filename=public_path('uploads').'/'.$fileName;
+//        QrCode::format('png')->size(200)->generate($url, $filename);
+//        return Storage::disk('admin')->url($fileName);
+//    }
+
     public function qrcode()
     {
         $data=\App\Models\Qrcode::find(1);
-        $url= url("/qr");
-        //随机二维码图片名
-        $fileName = 'my' . '.png';
-        //二维码图片路径
-        $filename=public_path('uploads').'/'.$fileName;
-        QrCode::format('png')->size(200)->generate($url, $filename);
-        return Storage::disk('admin')->url($fileName);
-    }
-
-    public function qr()
-    {
-        $data=\App\Models\Qrcode::find(1);
-        return view('qrcode', ['model' =>$data]);
+//        return view('qrcode', ['model' =>$data]);
+        return $this->success($data['text']);
     }
 
 }
