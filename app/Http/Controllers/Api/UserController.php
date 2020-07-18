88e30ef24683aa39912e9f423fdf8d827865cb10
1452073959@qq.com <1452073959@qq.com>
2020-07-18 19:31:04 +0800

    1

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Api\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
     //获取用户信息
@@ -18,9 +18,14 @@ class UserController extends Controller
     //修改用户信息
     public function updateuser(Request $request)
     {
-//        $user=$request->user();
+        $n=$request->all();
         $user = auth('api')->user();
-        $res= $user->update($request->all());
-        return $this->success($user);
+        $res=DB::table('users')->where('id',$user['id'])->update($n);
+        if($res){
+            return $this->success('修改成功');
+        }else{
+            return $this->failed('修改失败');
+        }
+
     }
 }
